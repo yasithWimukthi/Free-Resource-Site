6782cf58df90306d552c9bd5500def4dc2c926cb
yasith wimukthi <it19966922@my.sliit.lk>
2021-11-10 19:50:09 +0530

    implement save course

--- app/Http/Controllers/AwardingBodyController.php
+++ app/Http/Controllers/AwardingBodyController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AwardingBody;
 use Illuminate\Http\Request;
 
 class AwardingBodyController extends Controller
 {
-    //
+    public function store(Request $request){
+        $inputs = request()->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+
+
+        $awardingBody = new AwardingBody();
+        $awardingBody->name = $request->name;
+        $awardingBody->description = $request->description;
+
+
+        $awardingBody->save();
+
+        return back();
+    }
 }
