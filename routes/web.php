c38f8e4545d786d2f311f7bd690c841be17a8748
yasith wimukthi <it19966922@my.sliit.lk>
2021-11-25 19:12:16 +0530

    implement top nav bar

--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,9 @@ Route::get('/admin', function () {
 
 Route::get('/', Resource::class);
 
-//Route::get('/awarding-body', function () {
-//    return view('awardingbody');
-//});
 
 Route::get('/awarding-body', [App\Http\Controllers\AwardingBodyController::class, 'index'])->name('awardingbody.index');
 
-//Route::get('/course', function () {
-//    return view('course');
-//});
 
 Route::get('/course', [App\Http\Controllers\CourseController::class, 'index'])->name('course.index');
 Route::get('/document', [App\Http\Controllers\DocumentController::class, 'index'])->name('document.index');
@@ -36,10 +30,6 @@ Route::get('/document', [App\Http\Controllers\DocumentController::class, 'index'
 Route::get('/exam', function () {
     return view('exam');
 });
-
-//Route::get('/document', function () {
-//    return view('document');
-//});
 
 Route::post('/admin/exam', [App\Http\Controllers\ExamController::class, 'store'])->name('exam.store');
 Route::post('/admin/document', [App\Http\Controllers\DocumentController::class, 'store'])->name('document.store');
@@ -50,8 +40,5 @@ Route::get('/admin/awarding-body/delete/{id}', [App\Http\Controllers\AwardingBod
 
 Route::post('/admin/course', [App\Http\Controllers\CourseController::class, 'store'])->name('course.store');
 
-//Route::get('getCourses/{id}', 'CategoryController@get_causes_against_category');
-
-//Route::get('getCourses/{id}', [App\Http\Controllers\CourseController::class, 'getCoursesByAwardingId']);
 Route::get('/getCourses', [App\Http\Controllers\CourseController::class, 'getCoursesByAwardingId']);
 Route::get('/getCoursesById', [App\Http\Controllers\CourseController::class, 'getCoursesById']);
